dec15a05a46996cbe3a572e818356740ab514c1d
Martin Blomberg <martin.blomberg@outlook.com>
2023-03-28 11:38:35 +0200

    Add SubPub example

--- src/main/java/com/example/springbootdemo/service/Publisher.java
+++ src/main/java/com/example/springbootdemo/service/Publisher.java
@@ -1,40 +1,67 @@
 package com.example.springbootdemo.service;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
-
-import java.util.Arrays;
 
 @Service
 public class Publisher {
 
     RabbitTemplate template;
     String MESSAGE_QUEUE = "messages";
+    String EMAIL_QUEUE = "emails";
 
     public Publisher(RabbitTemplate template) {
         this.template = template;
     }
 
+//    public void publishMessage(String message) {
+//        template.convertAndSend(MESSAGE_QUEUE, message);
+//    }
     public void publishMessage(String message) {
-        template.convertAndSend(MESSAGE_QUEUE, message);
+        template.convertAndSend("my.topic","message.data", message);
     }
 
     @Bean
-    public Queue queue() {
+    public Queue messageQueue() {
         return new Queue(MESSAGE_QUEUE);
     }
+
+    @Bean
+    public Queue emailQueue() {
+        return new Queue(EMAIL_QUEUE);
+    }
+
+    @Bean
+    public TopicExchange exchange(){
+        return new TopicExchange("my.topic");
+    }
+
+    @Bean
+    public Binding messages(Queue messageQueue, TopicExchange exchange){
+        return BindingBuilder.bind(messageQueue).to(exchange).with("message.*");
+    }
+
+    @Bean
+    public Binding emails(Queue emailQueue, TopicExchange exchange){
+        return BindingBuilder.bind(emailQueue).to(exchange).with("message.email");
+    }
+
 
     @Bean
     public SimpleMessageListenerContainer container(ConnectionFactory connectionFactory) {
         SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
         container.setQueueNames(MESSAGE_QUEUE);
-        container.setMessageListener(message -> System.out.println("Incoming message: " + new String(message.getBody())));
+        container.setMessageListener(message -> {
+            System.out.println("Message: " + new String(message.getBody()));
+        });
         return container;
     }
 
